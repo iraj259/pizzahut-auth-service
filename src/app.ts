6b4ab16ebf135b6c6c2d5ed11e677415590946fd
iraj259 <irajj.259@gmail.com>
2026-02-12 13:15:29 +0000

    Basic express setup  with logger and error handler done

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import logger from './config/logger.ts'
+import { HttpError } from 'http-errors'
+import type { NextFunction, Request, Response } from 'express'; // types only
+
+const app = express()
+
+// if we make the function async the global error handler wont be able to catch it, the sol is to use next middleware but in v5 of express it handles this thing too
+app.get('/', (req, res,)=>{
+    res.send('welcome to the auth service')
+})
+
+// global error handler and should always be placed at last
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err:HttpError,req:Request,res:Response,next:NextFunction)=>{
+    logger.error(err.message)
+    const statusCode = err.statusCode || 500
+    res.status(statusCode).json({
+        errors:[
+            {
+                type:err.name,
+                msg:err.message,
+                path:'',
+                location:'',
+            }
+        ]
+    })
+})
+
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,17 @@
-function welcome(name: string) {
-  console.log("welcome" + name);
+import app from "./app.ts";
+import { Config } from "./config/index.ts";
+import logger from "./config/logger.ts";
+
+const startServer=()=>{
+    const PORT = Config.PORT
+    try {
+        app.listen(PORT,()=>{
+            logger.info(`port up on`, {port:PORT});   
+        })
+    } catch (error) {
+        console.error('error connecting to server', error);
+        process.exit(1)
+    }
 }
-welcome("iraj");
+
+startServer()

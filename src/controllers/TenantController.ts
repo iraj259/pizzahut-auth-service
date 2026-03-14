154ac4fe2ca5daf7e14f346e0c82e891e3be78d1
iraj259 <irajj.259@gmail.com>
2026-03-14 23:35:43 +0000

    Feat: tenants test added

--- src/controllers/TenantController.ts
+++ src/controllers/TenantController.ts
@@ -68,8 +68,6 @@ export class TenantController{
                 total: count,
                 data: tenants,
             });
-
-            res.json(tenants);
         } catch (err) {
             next(err);
         }
